4a3a2ce80e931a95144310010fb30e37615b203f
Gyaneshwar Kumar <gyaneshwarkumarmrj1@gmail.com>
2018-07-12 03:41:35 +0530

    Added new features

--- app/src/main/java/in/websuite/web/newser/MainActivity.java
+++ app/src/main/java/in/websuite/web/newser/MainActivity.java
@@ -1,13 +1,129 @@
 package in.websuite.web.newser;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    String NEWS_URL="https://newsapi.org/v2/top-headlines?country=in&apiKey=55b76cf25d2c497b88d573aaa47d0756";
+
+    ProgressDialog pd;
+
+    TextView head_tv;
+    ImageView head_img;
+
+    RecyclerView recyclerView;
+
+    NewsApi newsApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=(RecyclerView)findViewById(R.id.news_list);
+        head_img=(ImageView)findViewById(R.id.news_head_img);
+        head_tv=(TextView)findViewById(R.id.news_head_text);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+
+        MyVolley volley=new MyVolley();
+        volley.getVolleyData();
+
+    }
+    class MyVolley
+    {
+        MyVolley()
+        {
+            pd=new ProgressDialog(MainActivity.this,ProgressDialog.STYLE_SPINNER);
+            pd.setMessage("fetching data...");
+            pd.setCancelable(false);
+            pd.show();
+        }
+        void getVolleyData()
+        {
+            StringRequest request=new StringRequest(NEWS_URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    GsonBuilder builder=new GsonBuilder();
+                    Gson gson=builder.create();
+                    newsApi=gson.fromJson (response,NewsApi.class);
+                    recyclerView.setAdapter(new MyAdapter());
+
+                    head_tv.setText(newsApi.getArticles().get(0).getTitle());
+                    Glide.with(MainActivity.this).load(newsApi.getArticles().get(0).getUrlToImage()).into(head_img);
+
+
+                    pd.dismiss();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    pd.dismiss();
+                    Toast.makeText(MainActivity.this,"Unable to fetch data : "+error,Toast.LENGTH_LONG).show();
+                }
+            });
+
+            RequestQueue queue=Volley.newRequestQueue(MainActivity.this);
+            queue.add(request);
+        }
+    }
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.RecyclerHolder>
+    {
+
+        @Override
+        public RecyclerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater=LayoutInflater.from(MainActivity.this);
+            View view=inflater.inflate(R.layout.news_view,null);
+            return new RecyclerHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerHolder holder, int position) {
+            if(position<newsApi.getArticles().size()-1)
+            {
+                holder.mNews_tv.setText(newsApi.getArticles().get(position+1).getTitle().toString());
+            Glide.with(holder.mNews_img.getContext()).load(newsApi.getArticles().get(position+1).getUrlToImage()).into(holder.mNews_img);
+//                Glide.with(holder.mNews_img.getContext()).load(newsApi.getArticles().get(position).getUrl()).into(holder.mNews_img);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return newsApi.getArticles().size();
+        }
+
+        class RecyclerHolder extends RecyclerView.ViewHolder
+        {
+            TextView mNews_tv;
+            ImageView mNews_img;
+
+            public RecyclerHolder(View itemView) {
+                super(itemView);
+
+                mNews_img=(ImageView)itemView.findViewById(R.id.news_img);
+                mNews_tv=(TextView)itemView.findViewById(R.id.news_text);
+            }
+        }
     }
 }
---
+++ app/src/main/java/in/websuite/web/newser/NewsApi.java
@@ -0,0 +1,143 @@
+package in.websuite.web.newser;
+
+import java.util.List;
+
+public class NewsApi {
+
+
+    /**
+     * status : ok
+     * totalResults : 20
+     * articles : [{"source":{"id":null,"name":"Deccanchronicle.com"},"author":"AFP","title":"Wimbledon 2018: Kevin Anderson knocks out Roger Federer, reaches semis","description":"Eighth seed Anderson will play 2016 runner-up Milos Raonic or American ninth seed John Isner on Friday for a place in Sunday's final.","url":"https://www.deccanchronicle.com/sports/tennis/110718/roger-federer-kevin-anderson-wimbledon-2018-quarterfinal-tennis-london.html","urlToImage":"https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-9rbaudjm4t0l1a0jf4tu6v0np5-20180711231640.Medi.jpeg","publishedAt":"2018-07-11T17:50:01Z"},{"source":{"id":null,"name":"Newsnation.in"},"author":null,"title":"Nitish Kumar dilutes Bihar's strict anti-liquor law","description":"Bihar on Wednesday approved amendments to the Bihar Prohibition and Excise Act, 2016, changing some of its stricter provisions, which was earlier termed as \u2018draconian\u2019 by a section of the society.","url":"http://www.newsnation.in/india-news/nitish-kumar-dilutes-bihars-strict-anti-liquor-law-article-198421.html","urlToImage":"https://newsnation1.s3.amazonaws.com/images/2018/07/11/978858822-Untitled1_6.jpg","publishedAt":"2018-07-11T17:27:00Z"},{"source":{"id":null,"name":"Hindustantimes.com"},"author":"HT\u2009Correspondent","title":"Yogendra Yadav alleges income tax raid at sister's hospital in Rewari on PM Modi's behest","description":"Swaraj India\u2009leader Yogendra Yadav said the Narendra Modi-led government was targeting his family.","url":"https://www.hindustantimes.com/india-news/yogendra-yadav-says-i-t-dept-raided-sister-s-hospital-blames-modi-govt-for-trying-to-silence-him/story-OjhA1okdpc6w7xpi9V7gMM.html","urlToImage":"https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/07/11/Pictures/htmetro_415916ea-8509-11e8-a662-45bbb3f001dc.jpg","publishedAt":"2018-07-11T17:15:00Z"},{"source":{"id":"the-hindu","name":"The Hindu"},"author":"Reuters","title":"Germany is Russia's captive: Trump","description":"At NATO, he says Germany was wrong to support an $11-bn Baltic Sea pipeline to import Russian gas","url":"https://www.thehindu.com/news/international/germany-is-russias-captive-trump/article24392277.ece","urlToImage":"https://www.thehindu.com/news/international/sl5uvx/article24170450.ece/ALTERNATES/LANDSCAPE_615/TRUMPtr","publishedAt":"2018-07-11T16:59:59Z"},{"source":{"id":null,"name":"Dnaindia.com"},"author":"PTI","title":"SC throws shade at Eiffel Tower, says it looks like a TV tower","description":"The SC was comparing the Eiffel Tower to Taj Mahal.","url":"http://www.dnaindia.com/india/report-sc-throws-shade-at-eiffel-tower-says-it-looks-like-a-tv-tower-2636020","urlToImage":"http://static.dnaindia.com/sites/default/files/styles/full/public/2018/07/11/703554-eiffel-tower.jpg","publishedAt":"2018-07-11T16:57:00Z"},{"source":{"id":null,"name":"Ndtv.com"},"author":"","title":"Tata Harrier, Toyota India Recall, Honda CD 110 Dream DX, Jaguar Land Rover India Sales","description":"Tata Motors has officially revealed the name of its H5X SUV. It will be called the Harrier. The H5X SUV has been spotted testing in India multiple times and could be launched in India by 2019. Toyota India has issued a recall for the Innova Crysta and the For\u2026","url":"https://auto.ndtv.com/videos/tata-harrier-toyota-india-recall-honda-cd-110-dream-dx-jaguar-land-rover-india-sales-489087","urlToImage":"https://c.ndtvimg.com/3hs3y4bay7i_tata-harrier_640x480.jpg","publishedAt":"2018-07-11T16:35:39Z"},{"source":{"id":null,"name":"Indianexpress.com"},"author":"Reuters","title":"China says will hit back after US proposes fresh tariffs on $200 bln in goods","description":"The Chinese foreign ministry described Washington's threats as \"typical bullying\" and said China needed to counter-attack to protect its interests.","url":"https://indianexpress.com/article/world/china-says-will-hit-back-after-us-proposes-fresh-tariffs-on-200-bln-in-goods-5255609/","urlToImage":"https://images.indianexpress.com/2018/05/china_and_us_flag-759.jpg?w=759","publishedAt":"2018-07-11T16:31:34Z"},{"source":{"id":null,"name":"Ndtv.com"},"author":null,"title":"Ranchi Bishop Defends Missionaries of Charity In Baby Trafficking Case","description":"The Roman Catholic Church has come out in support of Mother Teresa's Missionaries of Charity days after members of its Ranchi unit were accused of involvement in child trafficking. A senior church official accused the police of hounding the nuns who have been\u2026","url":"https://www.ndtv.com/india-news/ranchi-bishop-defends-missionaries-of-charity-in-baby-trafficking-case-1881757","urlToImage":"https://c.ndtvimg.com/g97v3cmxk1_missionaries-of-charity_625x300.jpg","publishedAt":"2018-07-11T16:05:22Z"},{"source":{"id":"the-times-of-india","name":"The Times of India"},"author":null,"title":"Thai boys were sedated and stretchered from cave in dramatic rescue","description":"Rest of World News: The footage, released on the Thai Navy SEALs Facebook page, shows a complex operation with numerous divers -- both foreign and Thai -- using pulleys,","url":"https://timesofindia.indiatimes.com/world/rest-of-world/thai-navy-seals-footage-shows-sedated-group-carried-from-cave/articleshow/64950386.cms","urlToImage":"https://static.toiimg.com/thumb/msid-64950395,width-1070,height-580,imgsize-1268403,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg","publishedAt":"2018-07-11T15:21:28Z"},{"source":{"id":null,"name":"Ndtv.com"},"author":"NDTVSports.com","title":"World Cup Semi Final, Croatia vs England Live Score: Harry Kane Key As England Face Stern Croatia Test","description":"World Cup 2018 Semi-Final, Live Football Match Score, Croatia vs England: Both teams face off with the final spot in sight","url":"https://sports.ndtv.com/2018-fifa-world-cup/croatia-vs-england-fifa-world-cup-2018-football-semi-final-live-score-1881737","urlToImage":"https://c.ndtvimg.com/skd9f2b8l8_england-reuters_625x300.jpg","publishedAt":"2018-07-11T15:19:39Z"},{"source":{"id":null,"name":"Business-standard.com"},"author":"Press Trust of India","title":"Unnao case: CBI charges BJP MLA with raping minor girl","description":"BJP MLA Kuldeep Singh Sengar was today charged by the CBI with raping a minor girl at his residence at Makhi village in Unnao district of Uttar Pradesh on June 4 last year, officials said.\nThe CBI was handed over the case by the state government","url":"https://www.business-standard.com/article/pti-stories/unnao-case-cbi-charges-bjp-mla-with-raping-minor-girl-118071101188_1.html","urlToImage":"https://bsmedia.business-standard.com/include/_mod/site/html5/images/no-meta-preview.jpg","publishedAt":"2018-07-11T15:06:00Z"},{"source":{"id":null,"name":"Ndtv.com"},"author":null,"title":"'Acted As Predators': Court On 3 Kerala Priests Accused Of Rape","description":"Three of the four Kerala priests accused of raping a woman for years failed to get protection from arrest today with the high court observing that the priests appeared to have \"acted as predators\". The judge said there was no reason for the court to ignore th\u2026","url":"https://www.ndtv.com/kerala-news/4-kerala-priests-accused-of-raping-woman-for-years-to-be-arrested-1881460","urlToImage":"https://i.ndtvimg.com/i/2018-04/sex-worker-istock-650_650x400_71524141653.jpg","publishedAt":"2018-07-11T14:55:00Z"},{"source":{"id":null,"name":"Business-standard.com"},"author":"Press Trust of India","title":"Pro-Left outfit to hold seminar series on Ramayana; CPI(M)","description":"As yet another 'Ramayana  month' is around the corner, a Kerala-based outfit of Sanskrit  scholars and historians are gearing up to hold state-wide  seminars on the epic to counter the alleged attempt by the  Sangh Parivar to 'misinterpret' ancient","url":"https://www.business-standard.com/article/pti-stories/pro-left-outfit-to-hold-seminar-series-on-ramayana-cpi-m-118071101084_1.html","urlToImage":"https://bsmedia.business-standard.com/include/_mod/site/html5/images/no-meta-preview.jpg","publishedAt":"2018-07-11T14:17:02Z"},{"source":{"id":null,"name":"Ndtv.com"},"author":null,"title":"Net Neutrality Gets Telecom Commission's Nod, New Telecom Policy Cleared","description":"","url":"https://gadgets.ndtv.com/telecom/news/net-neutrality-gets-telecom-commissions-nod-new-telecom-policy-cleared-1881685","urlToImage":"https://i.gadgets360cdn.com/large/tower_pixabay_68_1484652849826.jpeg","publishedAt":"2018-07-11T13:21:07Z"},{"source":{"id":null,"name":"Newindianexpress.com"},"author":null,"title":"Jayant Sinha expresses 'regret' over garlanding lynching convicts","description":"If by garlanding them an impression has gone out that I support such vigilantism then I express regret over it, Jayant Sinha said.","url":"http://www.newindianexpress.com/nation/2018/jul/11/jayant-sinha-expresses-regret-over-garlanding-lynching-convicts-1841753.html","urlToImage":"http://images.newindianexpress.com/uploads/user/imagelibrary/2018/7/11/w600X390/2015-07-27T075523Z_1_LYNXNPEB6Q09A_RTROPTP_3_INDIA-ECONOMY-FINMIN.png","publishedAt":"2018-07-11T12:39:00Z"},{"source":{"id":null,"name":"Mid-day.com"},"author":null,"title":"Sanjay Dutt to launch autobiography in 2019","description":"With the book, readers will get to look into the soul of Sanjay Dutt, through stories about his youth, his Bollywood stint in the 1980s and 1990s, his experiences in prison, and his process of self-discovery","url":"https://www.mid-day.com/articles/sanjay-dutt-to-launch-autobiography-in-2019/19597152","urlToImage":"https://images.mid-day.com/images/2018/jul/Sanjay-dutt_l.jpg","publishedAt":"2018-07-11T12:26:00Z"},{"source":{"id":"the-times-of-india","name":"The Times of India"},"author":"ET Online","title":"Indian Army commando killed as Kupwara sees heavy exchange of fire","description":"Initial reports suggest that an Indian Army commando has been killed in the line of duty and another personnel wounded.","url":"https://economictimes.indiatimes.com/news/defence/indian-army-commando-killed-as-kupwara-sees-heavy-exchange-of-fire/articleshow/64947012.cms","urlToImage":"https://img.etimg.com/thumb/msid-64947268,width-1070,height-580,imgsize-147785,overlay-etdefence/photo.jpg","publishedAt":"2018-07-11T11:48:00Z"},{"source":{"id":"the-times-of-india","name":"The Times of India"},"author":null,"title":"J&K: A minor killed, four injured in blast in Shopian","description":"India News: The explosion took place in the residential compound of Khursheed Ahmad Sheikh at Meemandar in Shopian district this afternoon, resulting in injuries","url":"https://timesofindia.indiatimes.com/india/jk-a-minor-killed-four-injured-in-blast-in-shopian/articleshow/64945936.cms","urlToImage":"https://static.toiimg.com/thumb/msid-64946034,width-1070,height-580,imgsize-1457783,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg","publishedAt":"2018-07-11T10:01:17Z"},{"source":{"id":null,"name":"Usnews.com"},"author":null,"title":"Pompeo Accuses Iran of Using Embassies for Terror Plots","description":null,"url":"https://www.usnews.com/news/world/articles/2018-07-10/pompeo-talks-tough-on-iran-while-visiting-the-emirates","urlToImage":null,"publishedAt":"2018-07-10T19:15:00Z"},{"source":{"id":null,"name":"Business-standard.com"},"author":"Press Trust of India","title":"Gandhi-Nehru family has dictatorial moorings: BJP","description":"Observing the 43rd anniversary of imposition of Emergency, the BJP today accused the Gandhi-Nehru family of having a \"dictatorial attitude\" with its members right from Jawaharlal Nehru to Rahul Gandhi having the feeling of exclusive entitlement to","url":"https://www.business-standard.com/article/pti-stories/gandhi-nehru-family-has-dictatorial-moorings-bjp-118062600882_1.html","urlToImage":"https://bsmedia.business-standard.com/include/_mod/site/html5/images/no-meta-preview.jpg","publishedAt":"2018-06-26T12:52:57Z"}]
+     */
+
+    private String status;
+    private int totalResults;
+    private List<ArticlesBean> articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<ArticlesBean> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<ArticlesBean> articles) {
+        this.articles = articles;
+    }
+
+    public static class ArticlesBean {
+        /**
+         * source : {"id":null,"name":"Deccanchronicle.com"}
+         * author : AFP
+         * title : Wimbledon 2018: Kevin Anderson knocks out Roger Federer, reaches semis
+         * description : Eighth seed Anderson will play 2016 runner-up Milos Raonic or American ninth seed John Isner on Friday for a place in Sunday's final.
+         * url : https://www.deccanchronicle.com/sports/tennis/110718/roger-federer-kevin-anderson-wimbledon-2018-quarterfinal-tennis-london.html
+         * urlToImage : https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-9rbaudjm4t0l1a0jf4tu6v0np5-20180711231640.Medi.jpeg
+         * publishedAt : 2018-07-11T17:50:01Z
+         */
+
+        private SourceBean source;
+        private String author;
+        private String title;
+        private String description;
+        private String url;
+        private String urlToImage;
+        private String publishedAt;
+
+        public SourceBean getSource() {
+            return source;
+        }
+
+        public void setSource(SourceBean source) {
+            this.source = source;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUrlToImage() {
+            return urlToImage;
+        }
+
+        public void setUrlToImage(String urlToImage) {
+            this.urlToImage = urlToImage;
+        }
+
+        public String getPublishedAt() {
+            return publishedAt;
+        }
+
+        public void setPublishedAt(String publishedAt) {
+            this.publishedAt = publishedAt;
+        }
+
+        public static class SourceBean {
+            /**
+             * id : null
+             * name : Deccanchronicle.com
+             */
+
+            private Object id;
+            private String name;
+
+            public Object getId() {
+                return id;
+            }
+
+            public void setId(Object id) {
+                this.id = id;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+}
